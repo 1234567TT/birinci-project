5b882c6b6ef4ccbc481cab1614f48a1105424fae
BESSAM ALSHEHABI <bessam.alshehabi@ogr.sakarya.edu.tr>
2022-05-10 17:39:51 +0300

    Hizmetler sayfasi

---
+++ KullanciHizmat.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+  <link rel="stylesheet" href="styleHizmet.css">
+  <title>Hizmetler</title>
+</head>
+<body>
+  </section>
+<section class="cards" id="services">
+  <h2 class="title">HİZMETLER</h2>
+<div class="content">
+<div class="card">
+  <div class="icon">
+    <i class="fas fa-laptop-code"></i> 
+   </div>
+  <div class="info">
+    <h3>Bilgisayar Uygulama</h3>
+    <p>Tüm bilgisayarlar ve telefonlar için web siteleri ve uygulamalar tasarlat</p>
+  </div>
+</div>
+<div class="card">
+  <div class="icon">
+    <i class="fas fa-wifi"></i>   </div>
+  <div class="info">
+    <h3>İletişim & Ağlar </h3>
+    <p>Ev internetinin harika fiyatlarla ve harika bir hızda yönetimi ve kurulumu</p>
+  </div>
+</div> 
+<div class="card">
+  <div class="icon">
+    <i class="far fa-user"></i>  </div>
+  <div class="info">
+    <h3>Çalışanlar </h3>
+    <p>Muhasebe, yönetim veya programlama alanında çalışan arıyorsanız tecrübeli çalışanlarımız var</p>
+  </div>
+</div> 
+</div>
+</section>
+<section class="cards contect" id="contact1">
+  <h2 class="title1">Birlikte çalışalım</h2>
+  <div class="content1">
+    <div class="card1">
+      <div class="icon1">
+        <i class="fas fa-envelope-square"></i>
+       </div>
+      <div class="info1">
+        <h3>Email</h3>
+        <p>bessamalshehabi<br>@gmail.com</p>
+      </div>
+    </div>
+    <div class="card1">
+      <div class="icon1">
+        <i class="fas fa-mobile-alt"></i>   </div>
+      <div class="info1">
+        <h3>Telefon </h3>
+        <p>+905524907408</p>
+      </div>
+
+</section>
+<footer class="f">
+<p>İyi Günler Dilerim <br><span>"BESSAM ALŞEHABİ"</span> </p>
+<div class="soc">
+  <a href="https://t.me/bessam1999" target="-blank"><i class="fab fa-telegram"></i></a> 
+      <a href="https://www.facebook.com/bessam.alshihabi.3" target="-blank"><i class="fab fa-facebook-square"></i></a>
+      <a href="https://twitter.com/AlshehabiBessam" target="-blank"><i class="fab fa-twitter-square"></i></a>
+      <a href="https://www.instagram.com/bessamalshehabi/" target="-blank"><i class="fab fa-instagram-square"></i></a>
+</div>
+
+</footer>
+</body>
+</html>
